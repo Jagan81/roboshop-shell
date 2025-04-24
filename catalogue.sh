edcf2200be5306e2765a2b6cef7d72c7719f6147
Jagannnnnn <jaganmohan85220@gmail.com>
2025-04-24 17:17:45 +0530

    roboshop code

--- catalogue.sh
+++ catalogue.sh
@@ -3,7 +3,7 @@ source common.sh
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-nodejs
+nodejs_app_setup
 
 dnf install mongodb-mongosh -y
 mongosh --host mongo-dev.jrdevops81.online </app/db/master-data.js
--- shipping.sh
+++ shipping.sh
@@ -1,22 +1,12 @@
 component=shipping
 source common.sh
 
+  maven_app_setup
 
-dnf install maven -y
-useradd roboshop
 
-cp shipping.service /etc/systemd/system/shipping.service
-
-artifact_download
-
-cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-dnf install mysql -y
+  dnf install mysql -y
 mysql -h mysql-dev.jrdevops81.online -uroot -pRoboShop@1 < /app/db/schema.sql
 mysql -h mysql-dev.jrdevops81.online -uroot -pRoboShop@1 < /app/db/app-user.sql
 mysql -h mysql-dev.jrdevops81.online -uroot -pRoboShop@1 < /app/db/master-data.sql
 
 
-systemd_setup
