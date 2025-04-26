ce30e76e7d0ff80672c82ca687638000aeabf616
Jagannnnnn <jaganmohan85220@gmail.com>
2025-04-26 13:32:13 +0530

    roboshop code

--- common.sh
+++ common.sh
@@ -1,60 +1,61 @@
 systemd_setup() {
   print_head Copy SystemD Service file
   cp $component.service /etc/systemd/system/$component.service &>> $log_file
-  echo $?
+  exit_status_print $?
 
  print_head Start Service
   systemctl daemon-reload &>> $log_file
   systemctl enable $component &>> $log_file
   systemctl restart $component &>> $log_file
-  echo $?
+  exit_status_print $?
 
 }
 
 artifact_download() {
   print_head Add Application user
   useradd roboshop &>> $log_file
-  echo $?
+  exit_status_print $?
 
 
   print_head Remove existing application code
   rm -rf /App &>> $log_file
-  echo $?
+  exit_status_print $?
 
   print_head create Application directory
   mkdir /App &>> $log_file
-  echo $?
+  exit_status_print $?
 
   print_head Download Application Content
   curl -L -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/component-v3.zip &>> $log_file
-  echo $?
+  exit_status_print $?
   cd /app
 
   print_head Extract Application Content
   unzip /tmp/$component.zip &>> $log_file
-  echo $?
+  exit_status_print $?
 }
 
 
 nodejs_app_seetup() {
   print_head Disable NodeJS default version
   dnf module disable nodejs -y &>> $log_file
-  echo $?
+  exit_status_print $?
 
   print_head Enable NodeJS 20
   dnf module enable nodejs:20 -y &>> $log_file
-  echo $?
+  exit_status_print $?
 
   print_head Install NodeJS
   dnf install nodejs -y &>> $log_file
-  echo $?
+  exit_status_print $?
 
   artifact_download
   cd /app
 
   print_head Install NodeJS Dependencies
   npm install &>> $log_file
-  echo $?
+  exit_status_print $?
+
 
   systemd_setup
 }
@@ -97,3 +98,11 @@ Print_head() {
 
 log_file=/tmp/roboshop.log
 rm -f $log_file
+
+exit_status_print() {
+  if [ $? -eq 0 ]; then
+      echo -e "\e[32m >> SUCCESS\e[0m"
+    else
+      echo -e "\e[31m >> FAILURE\e[0m"
+      fi
+}
