ebea8fa0fd2aa0138db3924053d47b4a886b622f
Jagannnnnn <jaganmohan85220@gmail.com>
2025-04-27 11:07:04 +0530

    roboshop code

--- frontend.sh
+++ frontend.sh
@@ -2,26 +2,35 @@ source common.sh
 
 print_head disable default nginx
 dnf module disable nginx -y &>> $log_file
+  exit_status_print $?
 
 print_head Enable nginx 24
 dnf module enable nginx:1.24 -y &>> $log_file
+  exit_status_print $?
 
 print_head Install nginx
 dnf install nginx -y &>> $log_file
+  exit_status_print $?
 
 print_head Copy nginx config file
 cp nginx.conf /etc/nginx/nginx.conf &>> $log_file
+  exit_status_print $?
 
 print_head Clean old content
 rm -rf /usr/share/nginx/html/* &>> $log_file
+  exit_status_print $?
 
 print_head Download app content
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>> $log_file
+  exit_status_print $?
+
 cd /usr/share/nginx/html
 
 print_head Extract app content
 unzip /tmp/frontend.zip &>> $log_file
+  exit_status_print $?
 
 print_head Start nginx service
 systemctl enable nginx &>> $log_file
 systemctl restart nginx &>> $log_file
+  exit_status_print $?
